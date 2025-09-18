474e45d208081f342050a4386288510a73200d25
VixonyAn <vixonyan@gmail.com>
2025-09-18 13:38:28 +0200

    Add project files.

---
+++ Bridge/Car.cs
@@ -0,0 +1,18 @@
+﻿namespace Bridge
+{
+    public class Car
+	{
+        public string Licenseplate { get; set; }
+        public DateTime Date { get; set; }
+
+        public double Price()
+        {
+            return 230;
+        }
+
+        public string VehicleType()
+        {
+            return "Car";
+        }
+    }
+}
