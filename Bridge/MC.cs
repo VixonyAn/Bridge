c2ecbc1f0cbec1361ce441cf1b3bec25cc2350f3
VixonyAn <vixonyan@gmail.com>
2025-09-18 13:59:11 +0200

    Task 3: implemented MC with two properties, two methods, and tested the methods in the TestClass

---
+++ Bridge/MC.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge
+{
+	public class MC
+	{
+		/// <summary>
+		/// A string of numbers and letters that make up a liscenseplate
+		/// </summary>
+		public string Licenseplate { get; set; }
+		/// <summary>
+		/// A date and time, used for time based functions like weekend discounts
+		/// </summary>
+		public DateTime Date { get; set; }
+
+		/// <summary>
+		/// A method that can be overwritten to add mathematical functions such as discounts
+		/// </summary>
+		/// <returns>Price as a double</returns>
+		public double Price()
+		{
+			return 120;
+		}
+
+		/// <summary>
+		/// A description of the vehicles type, such as car, truck, or motorcycle
+		/// </summary>
+		/// <returns>Vehicle type as a string</returns>
+		public string VehicleType()
+		{
+			return "MC";
+		}
+	}
+}
--- BridgeTest/BridgeTests.cs
+++ BridgeTest/BridgeTests.cs
@@ -29,5 +29,31 @@ namespace BridgeTest
 			//Assert
 			Assert.AreEqual("Car", vehicleType);
 		}
+
+		[TestMethod]
+		public void TestBridgePriceMC()
+		{
+			//Arrange
+			MC mc1 = new MC();
+
+			//Act
+			double price = mc1.Price();
+
+			//Assert
+			Assert.AreEqual(120, price);
+		}
+
+		[TestMethod]
+		public void TestVehicleTypeMC()
+		{
+			//Arrange
+			MC mc1 = new MC();
+
+			//Act
+			string vehicleType = mc1.VehicleType();
+
+			//Assert
+			Assert.AreEqual("MC", vehicleType);
+		}
 	}
 }
